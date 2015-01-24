39c21b058ce74f2fdcebb49a740cc2443fa96def
John Boehr <jbboehr@gmail.com>
2015-01-24 19:23:44 +0000

    tokenizer passing spec

--- tests/test_spec_handlebars_tokenizer.c
+++ tests/test_spec_handlebars_tokenizer.c
@@ -212,7 +212,7 @@ START_TEST(handlebars_spec_tokenizer)
     int token_int = 0;
     do {
         token_int = handlebars_yy_lex(&yylval_param, &yylloc_param, ctx->scanner);
-        if( token_int == 0 ) break;
+        if( token_int == END || token_int == INVALID ) break;
         YYSTYPE * lval = handlebars_yy_get_lval(ctx->scanner);
         
         // Make token object
