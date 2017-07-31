3350c98fbe1350388295b5e9462fce6518f49a44
John Boehr <jbboehr@gmail.com>
2017-07-31 14:52:53 -0700

    Fix compilation issue on linux

--- src/handlebars_opcode_serializer.c
+++ src/handlebars_opcode_serializer.c
@@ -26,13 +26,17 @@
 #include "handlebars_opcode_serializer.h"
 #include "handlebars_string.h"
 
+#define PTR_DIFF(a, b) ((size_t) (((char *) a) - ((char *) b)))
+#define PTR_ADD(a, b) ((void *) (((char *) a) + (b)))
 
+#define PATCH(ptr) ptr = (void *) (((char *) ptr) - ((char *) module->addr) + ((char *) module))
 
 static void * append(struct handlebars_module * module, void * source, size_t size)
 {
-    void * addr = (char *) module->data + module->data_size;
+    void * addr = PTR_ADD(module->data, module->data_size);
 #ifndef NDEBUG
-    size_t data_offset = (char *) module->data - (char *) module;
+    assert((void *) module->data > (void *) module);
+    size_t data_offset = PTR_DIFF(module->data, module);
     //fprintf(stderr, "Data offset: %ld, Data size: %ld, Append size: %ld, Buffer size: %ld\n", data_offset, module->data_size, size, module->size);
     assert(data_offset + module->data_size + size <= module->size);
 #endif
@@ -219,9 +223,9 @@ struct handlebars_module * handlebars_program_serialize(
     talloc_set_type(module, struct handlebars_module);
 
     // Setup pointers
-    module->programs = ((char *) module) + sizeof(struct handlebars_module);
-    module->opcodes = ((char *) module->programs) + (sizeof(struct handlebars_module_table_entry) * module->program_count);
-    module->data = ((char *) module->opcodes) + (sizeof(struct handlebars_opcode) * module->opcode_count);
+    module->programs =  PTR_ADD(module, sizeof(struct handlebars_module));
+    module->opcodes =  PTR_ADD(module->programs, (sizeof(struct handlebars_module_table_entry) * module->program_count));
+    module->data = PTR_ADD(module->opcodes, (sizeof(struct handlebars_opcode) * module->opcode_count));
 
     // Reset counts - use as index
     size_t program_count = module->program_count;
@@ -240,7 +244,6 @@ struct handlebars_module * handlebars_program_serialize(
 }
 
 
-#define PATCH(ptr) ptr = ((char *) ptr) - module->addr + (char *) module
 
 
 static inline void patch_operand(struct handlebars_module * module, struct handlebars_operand * operand)
