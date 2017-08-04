8f4af666f7a4eb189ab47b4e82dd87b9a7e9eeef
John Boehr <jbboehr@gmail.com>
2017-08-04 16:23:14 -0700

    Fix cmake on ubuntu

---
+++ cmake/FindLibYaml.cmake
@@ -0,0 +1,22 @@
+if (LibYaml_LIBRARIES AND LibYaml_INCLUDE_DIRS)
+    set (LibYaml_FIND_QUIETLY TRUE)
+endif (LibYaml_LIBRARIES AND LibYaml_INCLUDE_DIRS)
+
+find_path (LibYaml_INCLUDE_DIRS NAMES yaml.h)
+IF(MSVC)
+    add_definitions(-DYAML_DECLARE_STATIC)
+    find_library (LibYaml_LIBRARIES NAMES yaml_static)
+ELSE()
+    find_library (LibYaml_LIBRARIES NAMES yaml)
+ENDIF()
+
+if (LibYaml_LIBRARIES)
+    add_definitions(-DHAVE_LIBYAML)
+endif()
+
+include (FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LibYaml DEFAULT_MSG
+    LibYaml_LIBRARIES
+    LibYaml_INCLUDE_DIRS)
+
+mark_as_advanced(LibYaml_INCLUDE_DIRS LibYaml_LIBRARIES)
